0f030bd26433d0c7d3bb3f2b82dd76c605af911a
suhye0n <contact.suhyeon@gmail.com>
2022-11-29 18:23:12 +0900

    다이얼로그 완성

---
+++ Week12WPFDialog/Gundam.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Week12WPFDialog
+{
+    public partial class Gundam : Window
+    {
+        public Gundam()
+        {
+            InitializeComponent();
+            Name2.Focus();
+        }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+        public string MSName
+        {
+            get { return Name2.Text; }
+        }
+
+
+        public string MSModel
+        {
+            get { return Model.Text; }
+        }
+
+        public string MSParty => Party.Text;
+
+
+        private void OnOK(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void OnCancel(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}
--- Week12WPFDialog/MainWindow.xaml.cs
+++ Week12WPFDialog/MainWindow.xaml.cs
@@ -15,14 +15,28 @@ using System.Windows.Shapes;
 
 namespace Week12WPFDialog
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnAdd(object sender, RoutedEventArgs e)
+        {
+            Gundam g = new Gundam();
+            if (g.ShowDialog() != true)
+                return;
+
+            Result.Text += $"{g.MSParty}의 {g.MSModel} {g.MSName}{(HasJongsung(g.MSName) ? "이" : "가")} 추가되었습니다.\n";
+        }
+
+        public bool HasJongsung(string str)
+        {
+            if (str.Length < 1)
+                return true;
+            char last = str[str.Length - 1];
+            return (last - 44032) % 28 != 0 ? true : false;
         }
     }
 }
